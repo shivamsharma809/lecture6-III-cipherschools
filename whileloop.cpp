c7ce67cbe9615c7c392b68a75e6aec3b807d3526
shivamsharma809 <138153296+shivamsharma809@users.noreply.github.com>
2023-07-19 10:06:30 +0530

    Add files via upload

---
+++ whileloop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+
+   i=1;     // initialization 
+    while(i<101)
+    {
+     cout<<i<<" ";   // loop
+     i++;            // update
+    }
+}
